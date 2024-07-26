32d075403612e125339d38d82a9cc0c8912f7c09
Jalaj <jalaj.nagwani@crewman.in>
2024-07-26 11:58:39 +0530

    Made some changes in user create api

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,12 +4,6 @@ from rest_framework import serializers
 from .models import User, Company, Package, UserRole, UserProfile
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'last_login', 'date_joined')
-
-
 class CompanySerializer(serializers.ModelSerializer):
     total_user_count = serializers.SerializerMethodField()
 
@@ -39,3 +33,20 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+    role = UserRoleSerializer()
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'last_login', 'date_joined', 'is_staff', 'profile',
+                  'role']
+
+    def create(self, validated_data):
+        profile_data = validated_data.pop("profile")
+        role_data = validated_data.pop("role")
+        user = User.objects.create_user(**validated_data)
+        UserRole.objects.create(user=user, **role_data)
+        UserProfile.objects.create(user=user, **profile_data)
