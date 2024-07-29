4270430bf459a752f7820cf7f0e7ee733545cb97
Jalaj <jalaj.nagwani@crewman.in>
2024-07-29 16:10:32 +0530

    Added notice model and api

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,5 @@ router.register(r'user-profile', UserProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('user-permissions', UserPermissionsView.as_view(), name="user-permissions"),
     path('user-permissions/', UserPermissionsView.as_view(), name="user-permissions")
 ]
