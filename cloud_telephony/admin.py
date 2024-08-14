6ee7632c83a8c848957223b94c1e940a6d9750e1
shivam.kumar.sain <shivamkumar6399029@gmail.com>
2024-08-14 18:51:30 +0530

    Telephoney Assign Module

--- cloud_telephony/admin.py
+++ cloud_telephony/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CloudTelephonyChannel
+from .models import CloudTelephonyChannel,CloudTelephonyChannelAssign
 
 admin.site.register(CloudTelephonyChannel)
+admin.site.register(CloudTelephonyChannelAssign)
--- cloud_telephony/views.py
+++ cloud_telephony/views.py
@@ -5,9 +5,15 @@ from services.cloud_telephoney.cloud_telephoney_service import (
     deleteCloudTelephoneyChannel,
     updateCloudTelephoneyChannel,
     getCloudTelephoneyChannel,
+    createCloudTelephoneyChannelAssign,
+    updateCloudTelephoneyChannelAssign,
+    deleteCloudTelephoneyChannelAssign
 )
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CloudTelephonyChannelSerializer
+from .serializers import (
+    CloudTelephonyChannelSerializer,
+    CloudTelephonyChannelAssignSerializer,
+)
 import pdb
 
 from rest_framework.response import Response
@@ -79,12 +85,13 @@ class CloudTelephoneyChannelUpdate(APIView):
 
 class CloudTelephoneyChannelList(APIView):
     permission_classes = [IsAuthenticated]
+
     def get(self, request):
         try:
             data = getCloudTelephoneyChannel(request.user.id)
             serializer = CloudTelephonyChannelSerializer(data, many=True)
             return Response(
-                {"Success": True, "FollowUps": serializer.data},
+                {"Success": True, "Data": serializer.data},
                 status=status.HTTP_200_OK,
             )
         except Exception as e:
@@ -92,3 +99,71 @@ class CloudTelephoneyChannelList(APIView):
                 {"Success": False, "Error": str(e)},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
+class CloudtelephoneyChannelAssignForUser(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        try:
+            cloud_telephony_channel = createCloudTelephoneyChannelAssign(
+                request.data, request.user.id
+            )
+            if isinstance(cloud_telephony_channel, str):
+                return Response(
+                    {"Success": False, "Errors": cloud_telephony_channel},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            return Response(
+                {
+                    "Success": True,
+                    "data": CloudTelephonyChannelAssignSerializer(cloud_telephony_channel).data,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        except ValueError as e:
+            return Response(
+                {"Success": False, "Errors": str(e)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        except Exception as e:
+            return Response(
+                {"Success": False, "Errors": "An unexpected error occurred: " + str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
+
+class CloudTelephoneyChannelAssignUpdate(APIView):
+    permission_classes = [IsAuthenticated]
+    def put(self, request, id):
+        updatedData = updateCloudTelephoneyChannelAssign(id, request.data)
+        if updatedData:
+            return Response(
+                {
+                    "Success": True,
+                    "data": CloudTelephonyChannelAssignSerializer(updatedData).data,
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {
+                    "Success": False,
+                    "Error": "Not found or invalid data provided.",
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        
+class CloudTelephoneyChannelAssignDelete(APIView):
+    permission_classes = [IsAuthenticated]
+    def delete(self, request, id):
+        success = deleteCloudTelephoneyChannelAssign(id)
+        if success:
+            return Response(
+                {"Success": True, "Message": "Deleted successfully."},
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"Success": False, "Error": "Not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
