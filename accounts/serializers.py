0ebee39dda33c0ebb909d6e426369e5e91c7e99d
Jalaj <jalaj.nagwani@crewman.in>
2024-08-21 14:10:50 +0530

    Added self signup views and serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -83,3 +83,36 @@ class UserSerializer(serializers.ModelSerializer):
         UserProfile.objects.create(user=user, **profile_data)
 
         return user
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    role = UserRoleCreateSerializer()
+    company = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'password', 'email', 'role', 'company']
+
+    def create(self, validated_data):
+        role_data = validated_data.pop("role")
+        company_name = validated_data.pop("company")
+
+        # password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+        # print(password)
+
+        package = Package.objects.get(id=3)
+        company, created = Company.objects.get_or_create(name=company_name, package=package)
+
+        user = User.objects.create_user(**validated_data)
+        UserRole.objects.create(user=user, **role_data)
+
+        UserProfile.objects.create(
+            user=user,
+            contact_no="0000000000",
+            gender="m",
+            status=True,
+            marital_status="unmarried",
+            company=company
+        )
+
+        return user
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, CompanyViewSet, PackageViewSet, UserRoleViewSet, UserPermissionsView, UserProfileViewSet, \
-    NoticeViewSet, BranchViewSet
+    NoticeViewSet, BranchViewSet, AdminSelfSignUp
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet) 
@@ -14,5 +14,6 @@ router.register(r'branches', BranchViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('user-permissions/', UserPermissionsView.as_view(), name="user-permissions")
+    path('user-permissions/', UserPermissionsView.as_view(), name="user-permissions"),
+    path('self-signup/', AdminSelfSignUp.as_view(), name="self-signup"),
 ]
