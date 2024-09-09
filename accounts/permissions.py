0e1da648a86b2210da4ed27a82d7b710a1330434
shivam.kumar.sain <shivamkumar6399029@gmail.com>
2024-09-09 15:52:18 +0530

    leave approve or disapprove api

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -4,7 +4,10 @@ from guardian.shortcuts import get_objects_for_user
 
 
 class CanChangeCompanyStatusPermission(BasePermission):
-
     def has_object_permission(self, request, view, obj):
         pdb.set_trace()
         return request.user.has_perm('accounts.can_change_company_status')
+    
+class CanLeaveApproveAndDisapprove(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.has_perm('accounts.can_approve_disapprove_leave')
