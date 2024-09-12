73eccaf489a781b1c27e8eb8e5d6bfbe8e4fef97
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2024-09-12 16:10:34 +0530

    get menulist

--- superadmin_assets/serializers.py
+++ superadmin_assets/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from .models import MenuModel,SubMenuModel
 
-class MenuSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MenuModel
-        fields = '__all__'  
-
 
 class SubMenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubMenuModel
-        fields = '__all__'  
+        fields = '__all__'
+
+
+class MenuSerializer(serializers.ModelSerializer):
+    sub_menu_list = serializers.SerializerMethodField()
+
+    class Meta:
+        model = MenuModel
+        fields = '__all__'
+
+    def get_sub_menu_list(self, menu):
+        sub_menus = SubMenuModel.objects.filter(menu_id=menu.id)
+        return SubMenuSerializer(sub_menus, many=True).data
